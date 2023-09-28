5a0aa6025f460a0b5c61f08dfe69af161de3e3dc
ryuhanda <r.handa@everyleaf.com>
2023-09-29 04:09:27 +0900

    一覧画面へのアクセス時にログイン中ユーザーの日報を取得できるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+  TEAM = "everyleaf"
+
   def index
+    client = Esa::Client.new(access_token: current_user.access_token, current_team: TEAM)
+
+    response = client.posts(q: "@me category:日報")
+    @posts = response.body["posts"]
   end
 end
