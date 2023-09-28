ccfada64639d1c7c0973a9e5fdb846ba3c811c1e
ryuhanda <r.handa@everyleaf.com>
2023-09-28 20:15:41 +0900

    ログイン用のルーティングを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index]
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/auth/esa/callback", to: "sessions#create"
 end
