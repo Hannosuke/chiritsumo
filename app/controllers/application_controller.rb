d7db1ec8e4935831b5113c11e340916e2a0c2051
ryuhanda <r.handa@everyleaf.com>
2023-09-30 14:09:52 +0900

    本番環境でBasic認証を行う
    - 開発効率のため本番以外では行わない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+
+  before_action :basic_auth if Rails.env.production?
   before_action :require_login
 
   private
@@ -8,6 +10,12 @@ class ApplicationController < ActionController::Base
     return unless session[:user_id]
 
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV["BASIC_AUTH_NAME"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 
   def require_login
