80c4c9400cb64be5a64ad54a98fda259ab4b7080
ryuhanda <r.handa@everyleaf.com>
2023-10-03 20:23:10 +0900

    APIのページネーションを使って無限スクロール表示できるように記事を取得する
    - テンプレートで現在のページと次のページを使えるようにする
    - パラメータでページを指定してリクエストを送る
    - ユーザー体験が悪くならないよう1リクエストでの取得件数を30件にする
      - esa APIのデフォルトは20件

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,10 @@ class PostsController < ApplicationController
 
   def index
     client = Esa::Client.new(access_token: current_user.access_token, current_team: TEAM)
+    response = client.posts(q: "@me category:日報", per_page: 30, page: params[:page])
 
-    response = client.posts(q: "@me category:日報")
+    @current_page = response.body["page"]
+    @next_page = response.body["next_page"]
     @posts = response.body["posts"]
   end
 end
